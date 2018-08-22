c9d12e9d1aac0833d363fa962b0910f3b05ed5d1
Eric Boshart <eric@homestarts.com>
2018-08-22 15:49:05 -0400

    added loupe"

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import "./App.css";
 // Import your component from ./homestars-icons/index.js.
 // It is critical that you export it from that file
 // or else it won't be accessible from the package
-import { CircleBinoculars } from "./homestars-icons/index";
+import { CircleLoupe } from "./homestars-icons/index";
 
 class App extends Component {
   render() {
     return pug`
-      CircleBinoculars(size=200)
+      CircleLoupe(size=200)
     `;
   }
 }
