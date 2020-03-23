77396df64535fb815b7494b32cd0400e4ea34851
brookestrodel <brooke.strodel@gmail.com>
2020-03-23 16:32:04 -0500

    reactify the attributes

--- src/homestars-icons/icons/TaskClipboard.js
+++ src/homestars-icons/icons/TaskClipboard.js
@@ -5,26 +5,26 @@ import iconWrapper from "../iconWrapper";
 function TaskClipboard() {
   return (
     <svg width="22px" height="22px" viewBox="0 0 50 68">
-      <g id="Edit-Listing-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="Edit-Listing-v2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Dashboard-v2.1---Company-Profile---Categories-and-Tasks---add-category-modal" transform="translate(-697.000000, -258.000000)">
           <g id="Help-Modal" transform="translate(-1.000000, 2.000000)">
             <g id="icn-taskclipboard-2" transform="translate(698.000000, 257.000000)">
-              <ellipse id="Shadow" fill-opacity="0.049999997" fill="#000000" cx="23" cy="65" rx="23" ry="2"></ellipse>
+              <ellipse id="Shadow" fillOpacity="0.049999997" fill="#000000" cx="23" cy="65" rx="23" ry="2"></ellipse>
               <g id="Group-17" transform="translate(1.000000, 0.000000)">
-                  <polygon id="Stroke-7" stroke="#535455" stroke-width="2" fill="#C1C2C3" stroke-linecap="round" stroke-linejoin="round" points="32 8 32 14 10 14 10 8 0 8 0 58 42 58 42 8"></polygon>
-                  <path d="M32,14 L10,14 L10,8 C10,6.896 10.896,6 12,6 L30,6 C31.104,6 32,6.896 32,8 L32,14 Z" id="Stroke-9" stroke="#535455" stroke-width="2" fill="#EDEEEF" stroke-linecap="round" stroke-linejoin="round"></path>
-                  <polygon id="Stroke-11" stroke="#535455" stroke-width="2" fill="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" points="32 12 32 14 10 14 10 12 4 12 4 54 38 54 38 12"></polygon>
-                  <path d="M26,6 L16,6 L16,5 C16,2.238 18.239,0 21,0 C23.761,0 26,2.238 26,5 L26,6 Z" id="Stroke-15" stroke="#535455" stroke-width="2" fill="#EDEEEF" stroke-linecap="round" stroke-linejoin="round"></path>
+                  <polygon id="Stroke-7" stroke="#535455" strokeWidth="2" fill="#C1C2C3" strokeLinecap="round" strokeLinejoin="round" points="32 8 32 14 10 14 10 8 0 8 0 58 42 58 42 8"></polygon>
+                  <path d="M32,14 L10,14 L10,8 C10,6.896 10.896,6 12,6 L30,6 C31.104,6 32,6.896 32,8 L32,14 Z" id="Stroke-9" stroke="#535455" strokeWidth="2" fill="#EDEEEF" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <polygon id="Stroke-11" stroke="#535455" strokeWidth="2" fill="#FFFFFF" strokeLinecap="round" strokeLinejoin="round" points="32 12 32 14 10 14 10 12 4 12 4 54 38 54 38 12"></polygon>
+                  <path d="M26,6 L16,6 L16,5 C16,2.238 18.239,0 21,0 C23.761,0 26,2.238 26,5 L26,6 Z" id="Stroke-15" stroke="#535455" strokeWidth="2" fill="#EDEEEF" strokeLinecap="round" strokeLinejoin="round"></path>
                   <circle id="Oval" fill="#535455" cx="21" cy="3" r="1"></circle>
               </g>
-              <polyline id="Stroke-1" stroke="#2FBB86" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" points="29 24 31 26 34 21"></polyline>
-              <line x1="11" y1="24" x2="23" y2="24" id="Stroke-2" stroke="#DEDFE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></line>
-              <polyline id="Stroke-3" stroke="#2FBB86" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" points="29 34 31 36 34 31"></polyline>
-              <line x1="11" y1="34" x2="23" y2="34" id="Stroke-4" stroke="#DEDFE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></line>
+              <polyline id="Stroke-1" stroke="#2FBB86" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" points="29 24 31 26 34 21"></polyline>
+              <line x1="11" y1="24" x2="23" y2="24" id="Stroke-2" stroke="#DEDFE0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></line>
+              <polyline id="Stroke-3" stroke="#2FBB86" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" points="29 34 31 36 34 31"></polyline>
+              <line x1="11" y1="34" x2="23" y2="34" id="Stroke-4" stroke="#DEDFE0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></line>
               <rect id="Rectangle" stroke="#DEDFE0" fill="#FFFFFF" x="28.5" y="41.5" width="5" height="5" rx="2"></rect>
-              <line x1="11" y1="44" x2="23" y2="44" id="Stroke-6" stroke="#DEDFE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></line>
+              <line x1="11" y1="44" x2="23" y2="44" id="Stroke-6" stroke="#DEDFE0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></line>
               <circle id="Oval" stroke="#209468" fill="#2FBB86" cx="40" cy="53" r="9.5"></circle>
-              <g id="Group-6" transform="translate(36.000000, 50.000000)" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.408">
+              <g id="Group-6" transform="translate(36.000000, 50.000000)" stroke="#FFFFFF" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.408">
                   <line x1="4" y1="0" x2="4" y2="7" id="Path-2"></line>
                   <line x1="4" y1="-0.0555555556" x2="4" y2="7.05555556" id="Path-2" transform="translate(4.000000, 3.500000) rotate(-270.000000) translate(-4.000000, -3.500000) "></line>
               </g>
